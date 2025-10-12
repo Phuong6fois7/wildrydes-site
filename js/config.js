ff65babf174bc4d19e2fa8b2aeecc7e0ac7a87ae
Phuong6fois7 <124179733+Phuong6fois7@users.noreply.github.com>
2025-10-12 11:55:00 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_gsaYfTfmi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6a0pl0nbpkafm7l34d3n8h00nn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
